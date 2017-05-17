eb199d32c1c0eab2e138741bac593bb450e91d34
Glenn Bishop Jr <sifelaver@gmail.com>
2017-05-17 19:42:34 -0400

    Add project files.

---
+++ TalkToMe/Events/SpeechEvents.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using System.Speech.Recognition;
+
+namespace TalkToMe.Events
+{
+    public static class SpeechEvents
+    {
+
+        public static void recognizer_recognitionDebug(object sender, SpeechRecognizedEventArgs spe)
+        {
+            const string logPath = @"C:\SpeechRecognition\AudioSamples\Logs\log.txt";
+            const string path = @"C:\SpeechRecognition\AudioSamples\dbugaudio.wav";
+
+            using (var streamWriter = new StreamWriter(logPath,true))
+            {
+                WriteLine(DateTime.Now.ToLongDateString());
+                WriteLine("**** DEBUG ****");
+                WriteLine("Confidence level: " + spe.Result.Confidence);
+                WriteLine("Possible alternatives: " + spe.Result.Alternates);
+
+                foreach (var alternative in spe.Result.Alternates)
+                {
+                    WriteLine("Alternative: " + alternative);
+                }
+                WriteLine("Actual: " + spe.Result.Text);
+
+                foreach (var word in spe.Result.Words)
+                {
+                    WriteLine("Words so far: " + word.Text);
+                    WriteLine("Confidence level: " + word.Confidence);
+                    WriteLine("Display Attributes: " + word.DisplayAttributes);
+                    WriteLine("Pronunciation: " + word.Pronunciation);
+                }
+
+                WriteLine("Dumping audio...");
+                streamWriter.Close();
+            }
+            
+            var nameAudio = spe.Result.Audio;
+
+            using (Stream outputStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+            {
+                nameAudio?.WriteToWaveStream(outputStream);
+                outputStream.Close();
+            }
+        }
+
+        public static void recognizer_recognizedSpeech(object sender, SpeechRecognizedEventArgs spe)
+        {
+            WriteLine("Recognized " + spe.Result.Text);
+        }
+
+        public static void recognizer_IfRecognizedCorrectWords(object sender, SpeechRecognizedEventArgs spe)
+        {
+            switch (spe.Result.Text)
+            {
+                case "run":
+                    WriteLine("Run Forrest run!");
+                    break;
+                case "open garage":
+                    WriteLine("Garage door OPENED");
+                    break;
+                case "close garage":
+                    Write("Garage door CLOSED");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ TalkToMe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+using System.Speech;
+using System.Speech.Recognition;
+using TalkToMe.Events;
+
+namespace TalkToMe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var recognitionEngine = new SpeechRecognitionEngine(new CultureInfo("en-US")))
+            {
+                recognitionEngine.LoadGrammar(new DictationGrammar());
+                recognitionEngine.SpeechRecognized += SpeechEvents.recognizer_IfRecognizedCorrectWords;
+                recognitionEngine.SpeechRecognized += SpeechEvents.recognizer_recognitionDebug;
+
+                recognitionEngine.SetInputToDefaultAudioDevice();
+
+                recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+                while (true)
+                {
+                    WriteLine("Talk to me.");
+                    ReadLine();
+                }
+            }
+
+        }
+    }
+}
